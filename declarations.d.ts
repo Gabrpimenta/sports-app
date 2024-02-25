bb962de56e6b76079ae955a3ddf214ead102ceca
Gabriel Pimenta <gabriel.pimenta@sistemairis.com.br>
2024-02-25 17:14:30 -0300

    [FEAT]: feature SVG configurations

---
+++ declarations.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png'; // para importar imagens e reconhecer modulo ts
+
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+
+  const content: React.FC<SvgProps>;
+  export default content;
+}
